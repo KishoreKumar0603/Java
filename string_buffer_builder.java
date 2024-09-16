4c5b97fd00d275ea8566007e32f41dfbc729b2d2
KishoreKumar0603 <kishorekumars5643@gmail.com>
2024-09-16 21:38:08 +0530

    new file added

---
+++ string_buffer_builder.java
@@ -0,0 +1,22 @@
+public class string_buffer_builder {
+    public static void main(String[] args){
+        StringBuffer buffer = new StringBuffer("kishore");
+        System.out.println(buffer);
+        buffer.append(" kumar");
+        System.out.println(buffer);
+        buffer.insert(0,"S ");
+        System.out.println(buffer);
+        buffer.replace(0, 4, "@@@@");
+        System.out.println(buffer);
+        buffer.delete(0, 4);
+        System.out.println(buffer);
+        buffer.reverse();
+        System.out.println(buffer);
+
+
+        StringBuffer a = new StringBuffer();
+        System.out.println(a.capacity());
+        a.append("kishore kumar sfddsagdg");
+        System.out.println(a.capacity()); // (old capacity * 2) + 2
+    }
+}
