e36482f0dbef450d1877dccc57dc0f16629fa0a6
KishoreKumar0603 <kishorekumars5643@gmail.com>
2024-09-17 11:43:49 +0530

    math file added

---
+++ math_method.java
@@ -0,0 +1,16 @@
+
+public class math_method {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-33));
+        System.out.println(Math.min(11, 1));
+        System.out.println(Math.max(200, 02));
+        System.out.println(Math.sqrt(29));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(34.3));
+        System.out.println(Math.round(34.6));
+        System.out.println(Math.ceil(88.2));
+        System.out.println(Math.ceil(88.7));
+        System.out.println(Math.floor(88.344));
+    }
+}
